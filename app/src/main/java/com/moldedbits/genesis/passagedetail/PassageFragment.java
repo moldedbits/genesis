76ee4f9f5920f1c062050ff6c0aa27e9eb506f36
Anuj <anuj.middha@gmail.com>
2017-09-16 20:11:45 +0530

    popup for translation

--- app/src/main/java/com/moldedbits/genesis/passagedetail/PassageFragment.java
+++ app/src/main/java/com/moldedbits/genesis/passagedetail/PassageFragment.java
@@ -1,24 +1,28 @@
 package com.moldedbits.genesis.passagedetail;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import com.moldedbits.genesis.BaseFragment;
 import com.moldedbits.genesis.R;
 import com.moldedbits.genesis.models.response.PassageDetails;
-import com.moldedbits.genesis.models.response.Question;
-import com.moldedbits.genesis.models.response.TranslatableString;
-import com.moldedbits.genesis.widgets.QuestionView;
 import com.moldedbits.genesis.widgets.TranslatableTextView;
+
+import org.jetbrains.annotations.NotNull;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class PassageFragment extends BaseFragment {
+public class PassageFragment extends BaseFragment implements
+        TranslatableTextView.TranslatableClickListener {
 
     private PassageDetails passageDetails;
 
@@ -27,6 +31,11 @@ public class PassageFragment extends BaseFragment {
 
     @BindView(R.id.tv_passage)
     TranslatableTextView tvPassage;
+
+    @BindView(R.id.shadow)
+    View shadow;
+
+    private PopupWindow popupWindow;
 
     @Nullable
     @Override
@@ -43,6 +52,13 @@ public class PassageFragment extends BaseFragment {
         if (passageDetails != null) {
             populateData(passageDetails);
         }
+
+        shadow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hidePopup();
+            }
+        });
     }
 
     public void setPassage(PassageDetails passageDetails) {
@@ -57,6 +73,52 @@ public class PassageFragment extends BaseFragment {
         tvTitle.setText(details.getDisplayName());
 
         // Passage
+        tvPassage.setTranslatableClickListener(this);
         tvPassage.setText(details.getPassageText().getSpanish(), details.getSentences());
+    }
+
+    @Override
+    public void onClick(@NonNull String original, @NotNull String translation) {
+        View popupContent;
+        if (popupWindow == null) {
+            popupContent = LayoutInflater.from(getContext())
+                    .inflate(R.layout.popup_translation, null);
+
+            popupContent.findViewById(R.id.btn_close).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    hidePopup();
+                }
+            });
+
+            popupWindow = new PopupWindow(popupContent,
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        } else {
+            popupContent = popupWindow.getContentView();
+        }
+
+        ((TextView) popupContent.findViewById(R.id.text_original)).setText(original);
+        ((TextView) popupContent.findViewById(R.id.text_translation)).setText(translation);
+
+
+        showPopup();
+    }
+
+    private void showPopup() {
+        // Set an elevation value for popup window
+        // Call requires API level 21
+        if(Build.VERSION.SDK_INT>=21){
+            popupWindow.setElevation(5.0f);
+        }
+
+        popupWindow.showAtLocation(tvPassage, Gravity.CENTER, 0, 0);
+        shadow.setVisibility(View.VISIBLE);
+    }
+
+    private void hidePopup() {
+        if (popupWindow != null) {
+            popupWindow.dismiss();
+        }
+        shadow.setVisibility(View.GONE);
     }
 }
